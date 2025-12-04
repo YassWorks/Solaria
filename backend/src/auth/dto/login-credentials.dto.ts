3ff161624f484b44a99ec29a3d187caa615d3815
Aymen Abid <aymen.abid2023@gmail.com>
2025-12-04 19:09:38 +0100

    doc: add swagger login dto examples

--- backend/src/auth/dto/login-credentials.dto.ts
+++ backend/src/auth/dto/login-credentials.dto.ts
@@ -4,11 +4,11 @@ import { ApiProperty } from '@nestjs/swagger';
 export class LoginCredentialsDto {
   @IsEmail()
   @IsNotEmpty()
-  @ApiProperty({ example: '12345678' })
+  @ApiProperty({ example: 'user@example.com' })
   email: string;
 
   @IsNotEmpty()
   @MinLength(6)
-  @ApiProperty({ example: '12345678' })
+  @ApiProperty({ example: 'Password123!' })
   password: string;
 }
